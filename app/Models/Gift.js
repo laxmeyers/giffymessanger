45b9e9b38afb6ad6d3268ff20e5388c34bc77c60
Andrew Meyers <laxmeyers@gmail.com>
2023-02-09 15:46:59 -0700

    stub out card

--- app/Models/Gift.js
+++ app/Models/Gift.js
@@ -5,4 +5,17 @@ export class Gift {
         this.tag = data.tag
         this.url = data.url
     }
- }
+
+    get GifsTemplate() {
+        return `
+        <div class="col-3">
+          <div class="card my-2">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+              <p class="card-text">Some quick example text</p>
+            </div>
+          </div>
+        </div>
+        `
+    }
+}
