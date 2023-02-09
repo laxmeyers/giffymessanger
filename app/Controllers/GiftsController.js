b5fdcee38f6ecf082a50cc4c14b15599b8d3de94
Andrew Meyers <laxmeyers@gmail.com>
2023-02-09 15:55:17 -0700

    draws to page

--- app/Controllers/GiftsController.js
+++ app/Controllers/GiftsController.js
@@ -1,2 +1,27 @@
+import { appState } from "../AppState.js";
+import { giftsService } from "../Services/GiftsService.js";
+import { Pop } from "../Utils/Pop.js";
+import { setHTML } from "../Utils/Writer.js";
 
-export class GiftsController { }
+function drawGifts() {
+    let template = ''
+    appState.gifts.forEach(g => template += g.GifsTemplate)
+    setHTML('gift-container', template)
+}
+
+export class GiftsController { 
+
+    constructor() {
+        console.log('this work?');
+        this.getGifts()
+        appState.on('gifts', drawGifts)
+    }
+
+    async getGifts() {
+        try {
+            await giftsService.getGifts()
+        } catch (error) {
+            Pop.error(error)
+        }
+    }
+}
--- app/Services/GiftsService.js
+++ app/Services/GiftsService.js
@@ -1,3 +1,15 @@
-class GiftsService { }
+import { appState } from "../AppState.js";
+import { Gift } from "../Models/Gift.js";
+import { sandBoxApi } from "./AxiosService.js"
+
+class GiftsService {
+    async getGifts() {
+        const res = await sandBoxApi.get('api/gifts')
+        console.log(res.data);
+        let newGift = res.data.map(g => new Gift(g))
+        appState.gifts = newGift
+    }
+}
 
 
+export const giftsService = new GiftsService()
